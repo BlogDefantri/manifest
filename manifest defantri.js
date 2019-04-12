3dc8d4cf394b9e9e8052296fae814b0de3b37ec7
defantri <thefunthree@gmail.com>
2019-04-12 13:32:06 +0700

    Add files via upload

---
+++ manifest defantri.js
@@ -0,0 +1,38 @@
+{
+  "name": "defantri.com",
+  "short_name": "defantri.com",
+  "start_url": "https://www.defantri.com/",
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#C51162",
+  "description": "Catatan Calon Guru Matematika About Berita Edukasi, Matematika Dasar, Matematika SMA, Matematika SMP, Matematika SD, Cerita Motivasi, dan Cerita Sekolah",
+  "icons": [{
+    "src": "https://cdn.staticaly.com/img/3.bp.blogspot.com/-eOsiA2XOSeQ/WSjRdL0EF_I/AAAAAAAALRw/tUlo5gLLApk3d_JNy8eyz3uCK0M4k_qDwCLcB/s48/icon_defantri.png",
+    "sizes": "48x48",
+    "type": "image/png"
+  }, {
+    "src": "https://cdn.staticaly.com/img/3.bp.blogspot.com/-eOsiA2XOSeQ/WSjRdL0EF_I/AAAAAAAALRw/tUlo5gLLApk3d_JNy8eyz3uCK0M4k_qDwCLcB/s72/icon_defantri.png",
+    "sizes": "72x72",
+    "type": "image/png"
+  }, {
+    "src": "https://cdn.staticaly.com/img/3.bp.blogspot.com/-eOsiA2XOSeQ/WSjRdL0EF_I/AAAAAAAALRw/tUlo5gLLApk3d_JNy8eyz3uCK0M4k_qDwCLcB/s96/icon_defantri.png",
+    "sizes": "96x96",
+    "type": "image/png"
+  }, {
+    "src": "https://cdn.staticaly.com/img/3.bp.blogspot.com/-eOsiA2XOSeQ/WSjRdL0EF_I/AAAAAAAALRw/tUlo5gLLApk3d_JNy8eyz3uCK0M4k_qDwCLcB/s144/icon_defantri.png",
+    "sizes": "144x144",
+    "type": "image/png"
+  }, {
+    "src": "https://cdn.staticaly.com/img/3.bp.blogspot.com/-eOsiA2XOSeQ/WSjRdL0EF_I/AAAAAAAALRw/tUlo5gLLApk3d_JNy8eyz3uCK0M4k_qDwCLcB/s168/icon_defantri.png",
+    "sizes": "168x168",
+    "type": "image/png"
+  }, {
+    "src": "https://cdn.staticaly.com/img/3.bp.blogspot.com/-eOsiA2XOSeQ/WSjRdL0EF_I/AAAAAAAALRw/tUlo5gLLApk3d_JNy8eyz3uCK0M4k_qDwCLcB/s192/icon_defantri.png",
+    "sizes": "192x192",
+    "type": "image/png"
+  }, {
+    "src": "https://cdn.staticaly.com/img/3.bp.blogspot.com/-eOsiA2XOSeQ/WSjRdL0EF_I/AAAAAAAALRw/tUlo5gLLApk3d_JNy8eyz3uCK0M4k_qDwCLcB/s512/icon_defantri.png",
+    "sizes": "512x512",
+    "type": "image/png"
+  }]
+}
